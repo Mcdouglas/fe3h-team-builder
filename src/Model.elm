dd516e70e5114ecf3ea33f9e91031117d3bbecae
Knecht Pierrick <pierrick.knecht.pro@gmail.com>
2019-10-05 13:45:05 +0200

    Add model

---
+++ src/Model.elm
@@ -0,0 +1,76 @@
+module Model exposing (..)
+
+
+type Gender
+    = Male
+    | Female
+    | Any
+
+
+type Subject
+    = Sword
+    | Lance
+    | Axe
+    | Bow
+    | Brawling
+    | Reason
+    | Faith
+    | Authority
+    | HeavyArmor
+    | Riding
+    | Flying
+
+
+type Rank
+    = E
+    | Eplus
+    | D
+    | Dplus
+    | C
+    | Cplus
+    | B
+    | Bplus
+    | A
+    | Aplus
+    | S
+    | Splus
+
+
+type alias Study =
+    { id : Int, subject : Subject, rank : Rank }
+
+
+type alias Proficiency =
+    { id : Int, subject : Subject, bonus : Int }
+
+
+type alias Class =
+    { id : Int, name : String, experience : Int, proficiency : Proficiency, certificationRequirement : List Study, genderRequirement : Gender }
+
+
+type alias ClassSkill =
+    { id : Int, name : String, learningClass : Class }
+
+
+type alias StudySkill =
+    { id : Int, name : String, study : Study }
+
+
+type alias PassiveSkill =
+    { id : Int }
+
+
+type alias ActiveSkill =
+    { id : Int }
+
+
+type alias Character =
+    { id : Int, name : String }
+
+
+type alias CharacterBuild =
+    { idCharacter : Int, listPassiveSkill : List PassiveSkill, listActiveSkill : List ActiveSkill }
+
+
+type alias TeamBuilder =
+    { builds : List CharacterBuild, link : String }
