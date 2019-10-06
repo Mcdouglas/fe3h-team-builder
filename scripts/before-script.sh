7cdab7cf5b2c1b65e0c7b4c374aed17e59359aff
Knecht Pierrick <pierrick.knecht.pro@gmail.com>
2019-10-07 00:31:10 +0200

    remove before-script.sh

--- scripts/before-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ev
-
-sed -i "s/resourcesDirLocation \= \"..\/../resourcesDirLocation \= \"https:\/\/raw.githubusercontent.com\/Mcdouglas\/fe3h-team-builder\/master/g" ../bundle.js
