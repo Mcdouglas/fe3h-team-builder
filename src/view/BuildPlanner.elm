78fdb28e33b50928d988093b75cc8c68f09362a8
Knecht Pierrick <pierrick.knecht.pro@gmail.com>
2019-10-13 14:05:30 +0200

    Add bootstratp + css. Try to handle static resources with travis

---
+++ src/view/BuildPlanner.elm
@@ -0,0 +1,11 @@
+module BuildPlanner exposing (..)
+
+import DataBuilder exposing (Msg(..))
+import GlobalModel exposing (Model)
+import Html exposing (..)
+
+
+viewPlanner : Model -> Html Msg
+viewPlanner model =
+    div []
+        []
