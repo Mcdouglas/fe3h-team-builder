bb4dd8c8bac4765a08961e5ab6a02c45d13614f0
Knecht Pierrick <pierrick.knecht.pro@gmail.com>
2019-12-11 22:21:21 +0100

    Fix elm-format

--- src/view/TeamBuilder.elm
+++ src/view/TeamBuilder.elm
@@ -20,8 +20,8 @@ viewBuilder : Model -> Html Msg
 viewBuilder model =
     div []
         [ h3 [ class "text-light" ] [ text "Fire Emblem Three Houses - Team Builder!" ]
-        , h4 [] [ text (Url.toString model.url) ]
-        , h4 [] [ text (model.url.query |> Maybe.withDefault "...") ]
+
+        -- , h4 [] [ text (model.url.query |> Maybe.withDefault "...") ]
         , viewTeam model
         , modalCharacterPicker model
         , modalSkillPicker model
