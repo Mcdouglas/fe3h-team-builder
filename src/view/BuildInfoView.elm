4d56c08a62ca0818085d5e61bec4be38808f8397
Knecht Pierrick <pierrick.knecht.pro@gmail.com>
2019-12-15 20:17:44 +0100

    Improve buildInfo

--- src/view/BuildInfoView.elm
+++ src/view/BuildInfoView.elm
@@ -7,6 +7,11 @@ import GlobalMessage exposing (Msg(..))
 import GlobalModel exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Job exposing (getJobById)
+import JobView exposing (viewJob)
+import MasterySkill exposing (..)
+import NoDataView exposing (viewNoData)
+import StandardSkill exposing (..)
 import Study exposing (..)
 import StudyView exposing (..)
 
@@ -23,11 +28,15 @@ sectionBuildInfo model ( idx, build ) =
         , hidden build.hiddenInfo
         , id ("info-" ++ String.fromInt build.idCharacter)
         ]
-        [ sectionCharacter model idx character ]
+        [ sectionCharacter model character
+        , sectionPassiveSkill model build
+        , sectionActiveSkill model build
+        , sectionJob model build
+        ]
 
 
-sectionCharacter : Model -> Int -> Character -> Html Msg
-sectionCharacter model idx character =
+sectionCharacter : Model -> Character -> Html Msg
+sectionCharacter model character =
     let
         academicSkills =
             getAcademicSkills character.id
@@ -38,3 +47,60 @@ sectionCharacter model idx character =
             , div [ class "study-table-row" ] (Dict.values academicSkills |> List.map getAcademicSkill)
             ]
         ]
+
+
+sectionPassiveSkill : Model -> Build -> Html Msg
+sectionPassiveSkill model build =
+    let
+        skillIdToView skillId skillType =
+            if skillType == StandardType then
+                getStandardSkill skillId |> Maybe.andThen (\s -> getStudyById s.studyId) |> Maybe.map viewStudy |> Maybe.withDefault viewNoData |> List.singleton
+
+            else
+                let
+                    firstDiv =
+                        getMasterySkill skillId |> Maybe.andThen (\m -> getJobById (List.head m.jobIdList |> Maybe.withDefault 0)) |> Maybe.map (\j -> j.studyIdList) |> Maybe.withDefault [] |> List.map getStudyById |> List.map (\s -> Maybe.map viewStudy s |> Maybe.withDefault viewNoData)
+
+                    secondDiv =
+                        div [ class "arrow-down" ] []
+
+                    thirdDiv =
+                        getMasterySkill skillId |> Maybe.andThen (\m -> getJobById (List.head m.jobIdList |> Maybe.withDefault 0)) |> Maybe.map viewJob |> Maybe.withDefault viewNoData
+                in
+                firstDiv ++ [ secondDiv, thirdDiv ]
+
+        fakeData skillid skillType =
+            viewNoData
+    in
+    div [ class "passive-skill-section" ] (build.listPassiveSkill |> List.map (\( idx, skillId, skillType ) -> div [ class "skill-info" ] (skillIdToView skillId skillType)))
+
+
+sectionActiveSkill : Model -> Build -> Html Msg
+sectionActiveSkill model build =
+    let
+        skillIdToView skillId skillType =
+            if skillType == StandardType then
+                getStandardSkill skillId |> Maybe.andThen (\s -> getStudyById s.studyId) |> Maybe.map viewStudy |> Maybe.withDefault viewNoData |> List.singleton
+
+            else
+                let
+                    firstDiv =
+                        getMasterySkill skillId |> Maybe.andThen (\m -> getJobById (List.head m.jobIdList |> Maybe.withDefault 0)) |> Maybe.map (\j -> j.studyIdList) |> Maybe.withDefault [] |> List.map getStudyById |> List.map (\s -> Maybe.map viewStudy s |> Maybe.withDefault viewNoData)
+
+                    secondDiv =
+                        div [ class "arrow-down" ] []
+
+                    thirdDiv =
+                        getMasterySkill skillId |> Maybe.andThen (\m -> getJobById (List.head m.jobIdList |> Maybe.withDefault 0)) |> Maybe.map viewJob |> Maybe.withDefault viewNoData
+                in
+                firstDiv ++ [ secondDiv, thirdDiv ]
+
+        fakeData skillid skillType =
+            viewNoData
+    in
+    div [ class "active-skill-section" ] (build.listActiveSkill |> List.map (\( idx, skillId, skillType ) -> div [ class "skill-info" ] (skillIdToView skillId skillType)))
+
+
+sectionJob : Model -> Build -> Html Msg
+sectionJob model build =
+    div [ class "job-section" ] (getJobById build.jobId |> Maybe.map (\j -> j.studyIdList) |> Maybe.withDefault [] |> List.map getStudyById |> List.map (\s -> Maybe.map viewStudy s |> Maybe.withDefault viewNoData))
